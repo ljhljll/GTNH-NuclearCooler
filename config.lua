1c628dcccbf9d099f11e415348934adc4dbddd97
ljhljll <48038838+ljhljll@users.noreply.github.com>
2024-06-28 22:36:23 +0800

    Update config.lua
    修正钍燃料棒物品名

--- config.lua
+++ config.lua
@@ -54,7 +54,7 @@ return {
                 },
                 {
                     name = "gregtech:gt.Quad_Thoriumcell",
-                    changeName = "greatech:gt.Quad_ThoriumcellDep",
+                    changeName = "gregtech:gt.Quad_ThoriumcellDep",
                     dmg = -1,
                     count = 26,
                     slot = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 48, 50, 52, 54 }
