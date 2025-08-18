dd15e35ef061e23c0ae178bbf71214942c5006e2
ljhljll <1356008591@qq.com>
2025-08-18 16:22:46 +0800

    style: 增加代码注释

--- coolantcellThread.lua
+++ coolantcellThread.lua
@@ -14,10 +14,14 @@ local function getLatchRedstoneSingal()
     end
     return false
 end
-
+-- 运行核电堆
 local function runningReactorChamber(rc)
     while true do
+        -- 是否开启耐久及物品名检测
         local canCheck = true
+        -- 配置文件中如果开启全局电量检测(energyLatchRedstone==-1)
+        -- 并且该项配置开启了电量控制(rc.energy == true)
+        -- 则根据电量锁存器的状态开关核电
         if config.energyLatchRedstone ~= -1 and rc.energy then
             if not getLatchRedstoneSingal() then
                 action.stopReactorChamberByRc(rc)
